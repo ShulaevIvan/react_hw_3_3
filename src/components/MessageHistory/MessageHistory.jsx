a8ba8d5d56e1c93881c49bdaba7784a14fb78519
Shulaev Ivan <mail@ntdservice.ru>
2023-04-23 21:33:55 +0300

    initial

---
+++ src/components/MessageHistory/MessageHistory.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const MessageHistory = (props) => {
+    return (
+        <React.Fragment>
+            <ul>
+                {props.list.map((msg) => {
+                    if (msg.type === 'message') {
+                        return (
+                            <li key={msg.id}>
+                                <div className="message-data">
+                                    <span className="message-data-name"><i className="fa fa-circle online"></i> {msg.from.name}</span>
+                                    <span className="message-data-time">{msg.time}</span>
+                                </div>
+                                <div className="message other-message float-right">{msg.text}</div>
+                            </li>
+                            );
+                    }
+                    else if (msg.type === 'typing') {
+                        return (
+                            <li key={msg.id}>
+                                <div className="message-data">
+                                    <span className="message-data-name"><i className="fa fa-circle online"></i>{msg.from.name}</span>
+                                </div>
+                                <div>...</div>
+                            </li>
+                        )
+                      
+                    }
+                    return (
+                        <li key={msg.id}>
+                            <div className="message-data">
+                                <span className="message-data-name"><i className="fa fa-circle online"></i>{msg.from.name}</span>
+                                <span className="message-data-time">{msg.time}</span>
+                            </div>
+                            <div className="message my-message">{msg.text}</div>
+                        </li>
+                    );
+                })}
+            </ul>
+        </React.Fragment>
+    )
+}
+
+export default MessageHistory;
